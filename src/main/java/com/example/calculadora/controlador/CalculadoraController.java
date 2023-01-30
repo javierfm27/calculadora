bc3979979d0804760ee65cc4d7eb4df813586ca3
javierfm27 <fdezmorjavier@gmail.com>
2023-01-30 14:52:07 +0100

    Añadidos dos pruebas para la API Rest

--- src/main/java/com/example/calculadora/controlador/CalculadoraController.java
+++ src/main/java/com/example/calculadora/controlador/CalculadoraController.java
@@ -3,6 +3,8 @@ package com.example.calculadora.controlador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/example/calculadora/CalculadoraApplicationTests.java
+++ src/test/java/com/example/calculadora/CalculadoraApplicationTests.java
@@ -1,13 +1,77 @@
 package com.example.calculadora;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.catalina.loader.WebappLoader;
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(classes = CalculadoraApplication.class ,webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CalculadoraApplicationTests {
 
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	
+	@BeforeAll
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+	
+	
 	@Test
-	void contextLoads() {
+	public void pruebaSuma() throws Exception
+	{
+		MvcResult result = this.mockMvc.perform(post("/api/ejecuta?num1=1&num2=2&operacion=+")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		result.getResponse().getContentAsString().equals("3");
 	}
+	
+	@Test
+	public void pruebaResta() throws Exception
+	{
+		MvcResult result = this.mockMvc.perform(post("/api/ejecuta?num1=2&num2=1&operacion=-")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		result.getResponse().getContentAsString().equals("1");
+	}
+	
+
 
 }
